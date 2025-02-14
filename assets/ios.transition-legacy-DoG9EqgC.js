b4a96e4bdb090eb0779477d2fbc5fa38b46df51e
Lord-binns <vince.olemberio2004@gmail.com>
2025-02-14 09:03:07 +0800

    Updates

--- assets/ios.transition-legacy-DoG9EqgC.js
+++ assets/ios.transition-legacy-DoG9EqgC.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BkMdagOZ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-CKh8Kx9o.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
