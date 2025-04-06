0fd9f0ad8fa98693e1b740726269be969453e061
Lord-binns <vince.olemberio2004@gmail.com>
2025-04-06 23:48:31 +0800

    fixed utils connection to supabase

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,7 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
 
-import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-const supabaseUrl = 'https://xjugejzlrtxxwmzxqbmi.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqdWdlanpscnR4eHdtenhxYm1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4NTk0NDUsImV4cCI6MjA1ODQzNTQ0NX0.G4YukGj6j2QfCDqYEYgCyDv6IeNVKQWGwA5eLVQKRsM'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
