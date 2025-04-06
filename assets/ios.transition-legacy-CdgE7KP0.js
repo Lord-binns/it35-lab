24f5ed499a3418751eb9f8849502c4a07b9a3da1
Lord-binns <vince.olemberio2004@gmail.com>
2025-04-07 00:41:36 +0800

    Updates

--- assets/ios.transition-legacy-CdgE7KP0.js
+++ assets/ios.transition-legacy-CdgE7KP0.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DAopOf19.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-S_CtDkXW.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
